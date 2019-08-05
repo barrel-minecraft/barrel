f756c6da3db105da13ac625b46662a94522a4df8
Dhillon Wilkinson <dhillonwilk@gmail.com>
2019-08-05 19:32:52 +0100

    Implemented VarLongReader and Writer classes for dealing with Minecraft VarLong data types

--- Barrel/Protocol/VarLong/VarLongReader.cs
+++ Barrel/Protocol/VarLong/VarLongReader.cs
@@ -7,10 +7,42 @@ using System.Threading.Tasks;
 
 namespace Barrel.Protocol.VarLong
 {
+    /// <summary>
+    /// Reads Minecraft VarLong data types from a binary stream. 
+    /// </summary>
     class VarLongReader : DataTypeReader
     {
         public VarLongReader(BinaryReader reader) : base(reader)
         {
+        }
+
+        /// <summary>
+        /// Reads a VarLong from the binary stream. VarLongs are never longer than 10 bytes
+        /// to prevent the possibility of a Denial of Service (DoS) attack.
+        /// </summary>
+        /// <returns>Returns the VarInt value read</returns>
+        public long ReadVarLong()
+        {
+            int bytesRead = 0;
+            long result = 0;
+            byte b = 0;
+
+            do
+            {
+                // Read the byte
+                b = ReadByte();
+                int value = (b & 0b01111111);
+                result |= (value << (7 * bytesRead));
+
+                bytesRead++;
+                if (bytesRead > 10)
+                {
+                    // VarLong is limited to a maximum of 10 bytes
+                    throw new DataTypeRangeException("VarLong is too large");
+                }
+            } while ((b & 0b10000000) != 0);
+
+            return result;
         }
     }
 }
---
+++ Barrel/Protocol/VarLong/VarLongWriter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Barrel.Protocol.VarLong
+{
+    /// <summary>
+    /// Writes Minecraft VarLong data types to a binary stream.
+    /// </summary>
+    class VarLongWriter : DataTypeWriter
+    {
+        public VarLongWriter(BinaryWriter writer) : base(writer)
+        {
+
+        }
+
+        /// <summary>
+        /// Writes a VarLong to the binary stream.
+        /// </summary>
+        /// <param name="value">The VarLong value to write</param>
+        public void WriteVarLong(long value)
+        {
+            do
+            {
+                byte temp = (byte)(value & 0b01111111);
+
+                // NOTE: >>> means the sign bit is shifted with the rest of the number
+                value = (int)((uint)value >> 7); // value >>>= 7;
+
+                if (value != 0)
+                {
+                    temp |= 0b10000000;
+                }
+
+                WriteByte(temp);
+            } while (value != 0);
+        }
+    }
+}
